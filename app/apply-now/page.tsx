7c72d9addaf9125383741e4c2ef08aa9f26ac385
dewanwakilahmed <dewanwakilahmed@gmail.com>
2024-03-22 20:38:04 +0600

    Setup Forms to Email

--- app/apply-now/page.tsx
+++ app/apply-now/page.tsx
@@ -1,19 +1,38 @@
-'use client';
+"use client";
 
 // CSS Import
-import './apply-now-page.css';
-
-// UI Component Import
-import Button from '@/components/ui/button/Button';
+import "./apply-now-page.css";
 
 const ApplyNowPage = () => {
+  const handleSubmit = async (event: any) => {
+    event.preventDefault(); // Prevent default form submission
+
+    const formData = new FormData(event.target);
+
+    const response = await fetch(
+      "https://script.google.com/macros/s/AKfycbzUbpcL_p0vf7TdiaGmryFp8fsRy2kKKTQJmKNPJKOwLjTGSbXdyRvNwCCZ7OGOpHli/exec",
+      {
+        // Replace YOUR_WEB_APP_URL with your actual URL
+        method: "POST",
+        body: formData,
+      }
+    );
+
+    if (response.ok) {
+      alert("Application submitted. We will get back to you soon!");
+      event.target.reset(); // Optional: Reset form
+    } else {
+      alert("Failed to submit application. Please try again later.");
+    }
+  };
+
   return (
     <div className='apply-now-page'>
       <section
         className='apply-now-section'
         style={{
           backgroundImage: `url('/images/hero-bg/apply-now-bg.webp')`,
-          backgroundPosition: 'center',
+          backgroundPosition: "center",
         }}
       >
         <div className='bg-img-overlay' />
@@ -28,100 +47,92 @@ const ApplyNowPage = () => {
         </div>
 
         <div className='apply-now-content'>
-          <form className='apply-now-form' action=''>
+          <form className='apply-now-form' onSubmit={handleSubmit}>
             <div className='input-item-container'>
-              <label className='input-label' htmlFor='name'>
+              <label className='input-label' htmlFor='Name'>
                 Name
               </label>
               <input
                 className='input-item'
                 type='text'
-                id='name'
-                name='name'
+                id='Name'
+                name='Name'
                 placeholder='Name'
                 required
               />
             </div>
 
             <div className='input-item-container'>
-              <label className='input-label' htmlFor='businessName'>
+              <label className='input-label' htmlFor='BusinessName'>
                 Business Name
               </label>
               <input
                 className='input-item'
                 type='text'
-                id='businessName'
-                name='businessName'
+                id='BusinessName'
+                name='BusinessName'
                 placeholder='Business Name'
               />
             </div>
 
             <div className='input-item-container'>
-              <label className='input-label' htmlFor='desiredAmount'>
+              <label className='input-label' htmlFor='DesiredAmount'>
                 Desired Funding Amount ($)
               </label>
               <input
                 className='input-item'
                 type='text'
-                id='desiredAmount'
-                name='desiredAmount'
+                id='DesiredAmount'
+                name='DesiredAmount'
                 placeholder='Desired Amount ($)'
                 required
               />
             </div>
 
             <div className='input-item-container'>
-              <label className='input-label' htmlFor='avgMonthlyRevenue'>
+              <label className='input-label' htmlFor='MonthlyRevenue'>
                 Average Monthly Revenue ($)
               </label>
               <input
                 className='input-item'
                 type='text'
-                id='avgMonthlyRevenue'
-                name='avgMonthlyRevenue'
+                id='MonthlyRevenue'
+                name='MonthlyRevenue'
                 placeholder='Monthly Revenue ($)'
                 required
               />
             </div>
 
             <div className='input-item-container'>
-              <label className='input-label' htmlFor='email'>
+              <label className='input-label' htmlFor='Email'>
                 Email
               </label>
               <input
                 className='input-item'
                 type='email'
-                id='email'
-                name='email'
+                id='Email'
+                name='Email'
                 placeholder='Email'
               />
             </div>
 
             <div className='input-item-container'>
-              <label className='input-label' htmlFor='phone'>
+              <label className='input-label' htmlFor='PhoneNo'>
                 Phone
               </label>
               <input
                 className='input-item'
                 type='tel'
-                id='phone'
-                name='phone'
+                id='PhoneNo'
+                name='PhoneNo'
                 placeholder='Phone no'
                 required
               />
             </div>
 
-            <Button
-              className='apply-now-form-btn'
-              btnType='primary'
-              goTo='/apply-now'
-            >
-              <input
-                className='apply-now-form-submit'
-                type='submit'
-                value='Apply Now'
-              />
-            </Button>
+            <button type='submit' className='contact-us-form-btn btn primary'>
+              Apply Now
+            </button>
           </form>
         </div>
       </section>
--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,19 +1,38 @@
-'use client';
+"use client";
 
 // CSS Import
-import './contact-us-page.css';
-
-// UI Component Import
-import Button from '@/components/ui/button/Button';
+import "./contact-us-page.css";
 
 const ContactUsPage = () => {
+  const handleSubmit = async (event: any) => {
+    event.preventDefault(); // Prevent default form submission
+
+    const formData = new FormData(event.target);
+
+    const response = await fetch(
+      "https://script.google.com/macros/s/AKfycbw-vLAI7tjODT12LO0cfMtFfd3zkXADUSqHuqexowA5nd7K0qXUeBJEky5_SxgcJktL9Q/exec",
+      {
+        // Replace YOUR_WEB_APP_URL with your actual URL
+        method: "POST",
+        body: formData,
+      }
+    );
+
+    if (response.ok) {
+      alert("Thank you for your message. We will get back to you soon!");
+      event.target.reset(); // Optional: Reset form
+    } else {
+      alert("Failed to send message. Please try again later.");
+    }
+  };
+
   return (
     <div className='contact-us-page'>
       <section
         className='contact-us-section'
         style={{
           backgroundImage: `url('/images/hero-bg/contact-us-bg.webp')`,
-          backgroundPosition: 'center',
+          backgroundPosition: "center",
         }}
       >
         <div className='bg-img-overlay' />
@@ -27,71 +46,63 @@ const ContactUsPage = () => {
         </div>
 
         <div className='contact-us-content'>
-          <form className='contact-us-form' action=''>
+          <form className='contact-us-form' onSubmit={handleSubmit}>
             <div className='input-item-container'>
-              <label className='input-label' htmlFor='name'>
+              <label className='input-label' htmlFor='Name'>
                 Name
               </label>
               <input
                 className='input-item'
                 type='text'
-                id='name'
-                name='name'
+                id='Name'
+                name='Name'
                 placeholder='Name'
                 required
               />
             </div>
 
             <div className='input-item-container'>
-              <label className='input-label' htmlFor='email'>
+              <label className='input-label' htmlFor='Email'>
                 Email
               </label>
               <input
                 className='input-item'
                 type='email'
-                id='email'
-                name='email'
+                id='Email'
+                name='Email'
                 placeholder='Email'
               />
             </div>
 
             <div className='input-item-container'>
-              <label className='input-label' htmlFor='phone'>
+              <label className='input-label' htmlFor='PhoneNo'>
                 Phone
               </label>
               <input
                 className='input-item'
                 type='tel'
-                id='phone'
-                name='phone'
+                id='PhoneNo'
+                name='PhoneNo'
                 placeholder='Phone no'
                 required
               />
             </div>
 
             <div className='input-item-container'>
-              <label className='input-label' htmlFor='message'>
+              <label className='input-label' htmlFor='Message'>
                 Message
               </label>
               <textarea
                 className='input-item'
-                id='message'
-                name='message'
+                id='Message'
+                name='Message'
                 placeholder='Message'
               ></textarea>
             </div>
 
-            <Button
-              className='contact-us-form-btn'
-              btnType='primary'
-              goTo='/contact-us'
-            >
-              <input
-                className='contact-us-form-submit'
-                type='submit'
-                value='Send Message'
-              />
-            </Button>
+            <button type='submit' className='contact-us-form-btn btn primary'>
+              Send Message
+            </button>
           </form>
         </div>
       </section>
